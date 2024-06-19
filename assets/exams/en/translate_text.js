b010800d807847657c325fceabf58adee8809c37
Matthew Araujo <90223014+MatthewAraujo@users.noreply.github.com>
2024-06-19 20:04:22 -0300

    Update translate_text.js

--- assets/exams/en/translate_text.js
+++ assets/exams/en/translate_text.js
@@ -3,19 +3,16 @@ const { TranslationServiceClient } = require('@google-cloud/translate');
 
 class TextTransform {
   constructor() {
-    // Configurações padrão (substitua com suas configurações reais)
     this.projectId = '<PROJECT_ID>';
     this.location = 'global';
     this.sourceLanguageCode = 'en';
     this.targetLanguageCode = 'pt-br';
     
-    // Criação do cliente de serviço de tradução
     this.translationClient = new TranslationServiceClient();
   }
 
   async translate(text) {
     try {
-      // Construct request
       const request = {
         parent: `projects/${this.projectId}/locations/${this.location}`,
         contents: text,
@@ -25,7 +22,6 @@ class TextTransform {
         model: `projects/${this.projectId}/locations/${this.location}/model/general/translation-llm`
       };
 
-      // Run request
       const [response] = await this.translationClient.translateText(request);
 
       return response.translations[0].translatedText;
