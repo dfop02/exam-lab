85227e726f389278e4dba493037752e66cc63778
MatthewAraujo <matthewaraujo20@gmail.com>
2024-06-19 16:24:01 -0300

    refactor: able to apply pt-br

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,103 @@
 $(document).ready(function () {
-  let exams_path = 'https://api.github.com/repos/dfop02/exam-lab/git/trees/main?recursive=1'
-  let exams_available = [];
+  // Get exams available
 
+  getExams(document.documentElement.lang);
   // Collect availables exams from assets/exams on the project's repo
-  $.ajax({
-    url: exams_path,
-    type: 'GET',
-    success: function (res) {
-      res.tree.filter(f => f.path.includes('assets/exams/'))
-        .map(f => f.path.split('/').pop())
-        .forEach(exam => exams_available.push(exam))
-      addExams(exams_available);
-    }
+  $(".btn-close").click(function (e) {
+    e.preventDefault();
+    $(".modal").addClass("hidden");
+    $(".overlay").addClass("hidden");
   });
 
-  $('.btn-close').click(function (e) {
+  $(".overlay").click(function (e) {
     e.preventDefault();
-    $('.modal').addClass('hidden');
-    $('.overlay').addClass('hidden');
-  });
-
-  $('.overlay').click(function (e) {
-    e.preventDefault();
-    $('.modal').addClass('hidden');
-    $('.overlay').addClass('hidden');
+    $(".modal").addClass("hidden");
+    $(".overlay").addClass("hidden");
   });
 
   setLocale();
 });
 
+function getExams(language) {
+  let exams_path =
+    "https://api.github.com/repos/MatthewAraujo/exam-lab/git/trees/main?recursive=1";
+  let exams_available = [];
+
+  let path = "assets/exams/";
+  if (language) {
+    path += language + "/";
+  }
+  console.log(path);
+
+  $.ajax({
+    url: exams_path,
+    type: "GET",
+    success: function (res) {
+      res.tree
+        .filter((f) => f.path.includes(path))
+        .map((f) => f.path.split("/").pop())
+        .forEach((exam) => exams_available.push(exam));
+      addExams(exams_available);
+    },
+  });
+}
+
 function removeExtension(filename) {
-  return filename.substring(0, filename.lastIndexOf('.')) || filename;
+  return filename.substring(0, filename.lastIndexOf(".")) || filename;
 }
 
 function snakeToCapitalize(str) {
-  var arr = str.split('_');
+  var arr = str.split("_");
   for (var i = 0; i < arr.length; i++) {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
   }
-  return arr.join(' ');
+  return arr.join(" ");
 }
 
 function addExams(exams) {
   for (exam of exams) {
-    $('.exams').append(
-      `<a class="exam-btn" href="#" onclick="updateModal('${exam}')"><span class='exam'>${removeExtension(snakeToCapitalize(exam))}</span><a>`
+    $(".exams").append(
+      `<a class="exam-btn" href="#" onclick="updateModal('${exam}')"><span class='exam'>${removeExtension(
+        snakeToCapitalize(exam)
+      )}</span><a>`
     );
   }
 }
 
 function updateModal(exam) {
-  $('.modal-actions > a.random').attr('href', `random.html?exam=${exam}`);
-  $('.modal-actions > a.simulation').attr('href', `introduction.html?exam=${exam}`);
+  $(".modal-actions > a.random").attr("href", `random.html?exam=${exam}`);
+  $(".modal-actions > a.simulation").attr(
+    "href",
+    `introduction.html?exam=${exam}`
+  );
 
-  $('.modal').removeClass('hidden');
-  $('.overlay').removeClass('hidden');
+  $(".modal").removeClass("hidden");
+  $(".overlay").removeClass("hidden");
 }
 
 function switchLanguage(language) {
   document.documentElement.lang = language;
   let lang = `:lang(${language})`;
-  let show = '[lang]' + lang;
+  let show = "[lang]" + lang;
   let hide = `[lang]:not(${lang})`;
   document.querySelectorAll(hide).forEach(function (node) {
-    node.style.display = 'none';
+    node.style.display = "none";
   });
   document.querySelectorAll(show).forEach(function (node) {
-    node.style.display = 'unset';
+    node.style.display = "unset";
   });
+  removeOldExams();
+  getExams(language);
+}
+
+function removeOldExams() {
+  $(".exams").empty();
 }
 
 function setLocale() {
-  let optionValues = $('.label-select img').map((i, o) => o.title).toArray()
+  let optionValues = $(".label-select img")
+    .map((i, o) => o.title)
+    .toArray();
   let browser_lang = navigator.language;
 
   // Check if browser lang is supported
